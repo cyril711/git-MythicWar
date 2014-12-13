91381a09a7a6a83ac1f409693a1230232087882a
cyril711 <cyril.lam01@gmail.com>
2014-12-13 09:13:02 +0100

    Refactoring
    Modification des path pour les fichiers .ui
    AJout des icons utilisees en conf

--- compite_ui_rsc.bat
+++ compite_ui_rsc.bat
@@ -1,22 +1,20 @@
 set RCC= C:\Python34\Lib\site-packages\PyQt5\pyrcc5.exe
 set UIC= C:\Python34\Lib\site-packages\PyQt5\pyuic5.bat
 
-call %UIC% python_modules/View/explorer.ui > python_modules/View/ui_explorer_view.py
-call %UIC% python_modules/View/profil.ui > python_modules/View/ui_profil_view.py
-call %UIC% python_modules/View/book_container.ui > python_modules/View/ui_book_container.py
-call %UIC% python_modules/View/book_world_homepage.ui > python_modules/View/ui_book_world_homepage.py
-call %UIC% python_modules/View/book_world_main_page.ui > python_modules/View/ui_book_world_main_page.py
-call %UIC% python_modules/View/main_window.ui > python_modules/View/ui_main_window.py
-call %UIC% python_modules/View/book_world_army.ui > python_modules/View/ui_book_world_army.py
-call %UIC% python_modules/View/profil_widget.ui > python_modules/View/ui_profil_widget.py
-call %UIC% python_modules/View/kingdom_layout.ui > python_modules/View/ui_kingdom_layout.py
-call %UIC% python_modules/View/warrior_layout.ui > python_modules/View/ui_warrior_layout.py
-call %UIC% python_modules/View/book_warrior_homepage.ui > python_modules/View/ui_book_warrior_homepage.py
-call %UIC% python_modules/View/book_warrior_page.ui > python_modules/View/ui_book_warrior_page.py
-call %UIC% python_modules/View/explorer.ui > python_modules/View/ui_explorer_widget.py
-call %UIC% python_modules/View/dialog_save.ui > python_modules/View/ui_dialog_save.py
-call %UIC% python_modules/View/dialog_settings.ui > python_modules/View/ui_dialog_settigns.py
-call %UIC% python_modules/View/dialog_kingdom_choice.ui > python_modules/View/ui_dialog_kingdom_choice.py
+call %UIC% python_modules/main_view/explorer.ui > python_modules/main_view/ui_explorer_view.py
+call %UIC% python_modules/view/view_kingdom/book_world_homepage.ui > python_modules/view/view_kingdom/ui_book_world_homepage.py
+call %UIC% python_modules/view/view_kingdom/book_world_main_page.ui > python_modules/view/view_kingdom/ui_book_world_main_page.py
+call %UIC% python_modules/main_view/main_window.ui > python_modules/main_view/ui_main_window.py
+call %UIC% python_modules/view/view_heros/book_world_army.ui > python_modules/view/view_heros/ui_book_world_army.py
+REM call %UIC% python_modules/View/profil_widget.ui > python_modules/View/ui_profil_widget.py
+call %UIC% python_modules/view/view_kingdom/kingdom_layout.ui > python_modules/view/view_kingdom/ui_kingdom_layout.py
+call %UIC% python_modules/view/view_heros/warrior_layout.ui > python_modules/view/view_heros/ui_warrior_layout.py
+call %UIC% python_modules/view/view_heros/book_warrior_homepage.ui > python_modules/view/view_heros/ui_book_warrior_homepage.py
+call %UIC% python_modules/view/view_heros/book_warrior_page.ui > python_modules/view/view_heros/ui_book_warrior_page.py
+call %UIC% python_modules/main_view/explorer.ui > python_modules/main_view/ui_explorer_widget.py
+call %UIC% python_modules/main_view/dialog_save.ui > python_modules/main_view/ui_dialog_save.py
+call %UIC% python_modules/main_view/dialog_settings.ui > python_modules/main_view/ui_dialog_settigns.py
+call %UIC% python_modules/main_view/dialog_kingdom_choice.ui > python_modules/main_view/ui_dialog_kingdom_choice.py
 
 
 %RCC% resources.qrc -o python_modules/resources_rc.py
